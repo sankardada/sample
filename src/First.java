4ac3945257ec5aa9656d691b01a42b1cc08a82a1
my pc <my pc@DESKTOP-V1AG363>
2018-01-07 13:52:42 +0530

    project addedsample

---
+++ src/First.java
@@ -0,0 +1,14 @@
+
+public class First {
+
+	public static void main(String[] args) {
+	System.out.println("rave welcom java");
+	System.out.println("rave welcom java");
+	System.out.println("rave welcom java");
+	System.out.println("rave welcom java");
+                 
+	
+	
+	}
+
+}
